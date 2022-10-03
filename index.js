63841cafefb1fd5ed42052ac1d645d8224b20563
JD Runtsch <katman04@gmail.com>
2022-10-02 21:57:28 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+/* window.onclick = (e) => {
+    console.log(e.target.className);
+    if (e.target.id == 11 || e.target.id == 111 || e.target.id == 1111){
+        document.getElementById("1").style.display = "none";
+        document.getElementById("2").style.display = "flex";
+        var Asel = 1;
+    }
+    else if (e.target.id == 12 || e.target.id == 122 || e.target.id == 1222){
+        document.getElementById("1").style.display = "none";
+        document.getElementById("2").style.display = "flex";
+        var Asel = 2;
+    }
+    else if (e.target.id == 13 || e.target.id == 133 || e.target.id == 1333){
+        document.getElementById("1").style.display = "none";
+        document.getElementById("2").style.display = "flex";
+        var Asel = 3;
+    }
+    else if (e.target.id == 4 || e.target.id == 144 || e.target.id == 1444){
+        document.getElementById("1").style.display = "none";
+        document.getElementById("2").style.display = "flex";
+        var Asel = 4;
+    }
+} */
